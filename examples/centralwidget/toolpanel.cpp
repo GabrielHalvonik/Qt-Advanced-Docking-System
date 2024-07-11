697bce393d8be05f7a41f7bb3a8e7159a741e0bd
Gabriel Halvonik <gabriel.halvonik@escapemotions.com>
2024-07-11 23:35:03 +0200

    Sample widget minor adjustment

--- examples/centralwidget/toolpanel.cpp
+++ examples/centralwidget/toolpanel.cpp
@@ -6,11 +6,12 @@
 ToolPanel::ToolPanel()
 {
     layout = new QFormLayout();
-
+    
+    static int order = 0;
     for (int i = 0; i < 10; ++i) {
         auto slider = new QSlider();
         slider->setOrientation(Qt::Horizontal);
-        layout->addRow(QString("parameter %1").arg(i), slider);
+        layout->addRow(QString("parameter %1").arg(++order), slider);
     }
 
     setLayout(layout);
